0af782be28516cceecbf2481ccbcecba8edbbdba
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-08-19 07:54:00 +0000

    dump version:3
    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@59375 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Fluent/ConvertExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Fluent
+{
+  public static class ConvertExtensions
+  {
+
+    public static T Cast<T>( this object obj )
+    {
+      return (T) obj;
+    }
+
+    public static T ConvertTo<T>( this object value )
+    {
+      if ( Convertor<T>.castMethod != null )
+        return Convertor<T>.castMethod( value );
+
+
+      return (T) value;
+    }
+
+    static ConvertExtensions()
+    {
+      Convertor<short>.castMethod    = Convert.ToInt16;
+      Convertor<int>.castMethod      = Convert.ToInt32;
+      Convertor<long>.castMethod     = Convert.ToInt64;
+      Convertor<byte>.castMethod     = Convert.ToByte;
+      Convertor<ushort>.castMethod   = Convert.ToUInt16;
+      Convertor<uint>.castMethod     = Convert.ToUInt32;
+      Convertor<ulong>.castMethod    = Convert.ToUInt64;
+      Convertor<sbyte>.castMethod    = Convert.ToSByte;
+      Convertor<float>.castMethod    = Convert.ToSingle;
+      Convertor<double>.castMethod   = Convert.ToDouble;
+      Convertor<decimal>.castMethod  = Convert.ToDecimal;
+      Convertor<bool>.castMethod     = Convert.ToBoolean;
+      Convertor<DateTime>.castMethod = Convert.ToDateTime;
+      Convertor<string>.castMethod   = Convert.ToString;
+    }
+
+    private class Convertor<T>
+    {
+      public static Func<object, T> castMethod;
+    }
+
+
+  }
+}
---
+++ Ivony.Fluent/EnumerableExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Fluent
+{
+  public static class EnumerableExtension
+  {
+
+    public static void ForEach<T>( this IEnumerable<T> source, Action<T> action )
+    {
+      foreach ( var item in source )
+        action( item );
+    }
+  }
+}

b73804f604aabaf4e2a23ca2d565ad64a9864d42
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-08-19 08:23:26 +0000

    dump version:48
    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@59419 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Fluent/EnumerableExtension.cs
+++ Ivony.Fluent/EnumerableExtension.cs
@@ -68,16 +68,7 @@ namespace Ivony.Fluent
     public static IEnumerable<TSource> BindTo<TSource, TTarget>( this IEnumerable<TSource> source, IEnumerable<TTarget> targets, Action<TSource, TTarget, int> binder )
     {
 
-      using ( var sourceIterator = source.GetEnumerator() )
-      {
-        using ( var targetIterator = targets.GetEnumerator() )
-        {
-          int index = 0;
-
-          while ( sourceIterator.MoveNext() && targetIterator.MoveNext() )
-            binder( sourceIterator.Current, targetIterator.Current, index++ );
-        }
-      }
+      BindCore( source, targets, binder );
 
       return source;
     }
@@ -111,6 +102,109 @@ namespace Ivony.Fluent
     /// <returns>源集合</returns>
     public static IEnumerable<TSource> BindTo<TSource, TTarget>( this IEnumerable<TSource> source, IEnumerable<TTarget> targets, TSource defaultValue, Action<TSource, TTarget, int> binder )
     {
+      BindCore( source, targets, defaultValue, binder );
+
+      return source;
+    }
+
+
+
+
+
+    /// <summary>
+    /// 将源集合的每一项按顺序绑定到集合
+    /// </summary>
+    /// <typeparam name="TSource">源集合元素类型</typeparam>
+    /// <typeparam name="TTarget">目标集合元素类型</typeparam>
+    /// <param name="source">源集合</param>
+    /// <param name="targets">目标集合</param>
+    /// <param name="binder">绑定方法</param>
+    /// <returns>源集合</returns>
+    public static IEnumerable<TTarget> BindFrom<TSource, TTarget>( this IEnumerable<TTarget> targets, IEnumerable<TSource> source, Action<TSource, TTarget> binder )
+    {
+      return BindFrom( targets, source, ( s, t, i ) => binder( s, t ) );
+    }
+
+
+    /// <summary>
+    /// 将源集合的每一项按顺序绑定到集合
+    /// </summary>
+    /// <typeparam name="TSource">源集合元素类型</typeparam>
+    /// <typeparam name="TTarget">目标集合元素类型</typeparam>
+    /// <param name="source">源集合</param>
+    /// <param name="targets">目标集合</param>
+    /// <param name="binder">绑定方法</param>
+    /// <returns>源集合</returns>
+    public static IEnumerable<TTarget> BindFrom<TSource, TTarget>( this  IEnumerable<TTarget> targets, IEnumerable<TSource> source, Action<TSource, TTarget, int> binder )
+    {
+
+      BindCore( source, targets, binder );
+
+      return targets;
+    }
+
+
+    /// <summary>
+    /// 将源集合的每一项按顺序绑定到集合
+    /// </summary>
+    /// <typeparam name="TSource">源集合元素类型</typeparam>
+    /// <typeparam name="TTarget">目标集合元素类型</typeparam>
+    /// <param name="source">源集合</param>
+    /// <param name="targets">目标集合</param>
+    /// <param name="defaultValue">当源集合元素不够时所采用的默认元素</param>
+    /// <param name="binder">绑定方法</param>
+    /// <returns>源集合</returns>
+    public static IEnumerable<TTarget> BindFrom<TSource, TTarget>( this IEnumerable<TTarget> targets, IEnumerable<TSource> source, TSource defaultValue, Action<TSource, TTarget> binder )
+    {
+      return BindFrom( targets, source, defaultValue, ( s, t, i ) => binder( s, t ) );
+    }
+
+
+    /// <summary>
+    /// 将源集合的每一项按顺序绑定到集合
+    /// </summary>
+    /// <typeparam name="TSource">源集合元素类型</typeparam>
+    /// <typeparam name="TTarget">目标集合元素类型</typeparam>
+    /// <param name="source">源集合</param>
+    /// <param name="targets">目标集合</param>
+    /// <param name="defaultValue">当源集合元素不够时所采用的默认元素</param>
+    /// <param name="binder">绑定方法</param>
+    /// <returns>源集合</returns>
+    public static IEnumerable<TTarget> BindFrom<TSource, TTarget>( this IEnumerable<TTarget> targets, IEnumerable<TSource> source, TSource defaultValue, Action<TSource, TTarget, int> binder )
+    {
+      BindCore( source, targets, defaultValue, binder );
+
+      return targets;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    private static void BindCore<TSource, TTarget>( IEnumerable<TSource> source, IEnumerable<TTarget> targets, Action<TSource, TTarget, int> binder )
+    {
+
+      using ( var sourceIterator = source.GetEnumerator() )
+      {
+        using ( var targetIterator = targets.GetEnumerator() )
+        {
+          int index = 0;
+
+          while ( sourceIterator.MoveNext() && targetIterator.MoveNext() )
+            binder( sourceIterator.Current, targetIterator.Current, index++ );
+        }
+      }
+    }
+
+
+    private static void BindCore<TSource, TTarget>( IEnumerable<TSource> source, IEnumerable<TTarget> targets, TSource defaultValue, Action<TSource, TTarget, int> binder )
+    {
 
       using ( var sourceIterator = source.GetEnumerator() )
       {
@@ -132,12 +226,9 @@ namespace Ivony.Fluent
 
             binder( dataItem, targetItem, index++ );
 
-
           }
         }
       }
-
-      return source;
     }
 
   }

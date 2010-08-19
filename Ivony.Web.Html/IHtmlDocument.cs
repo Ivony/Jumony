ef6851fedce364c33a644000e8bedf1f8ea06afc
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-08-19 08:05:01 +0000

    dump version:38
    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@59408 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Web.Html/IHtmlDocument.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Web.Html
+{
+  public interface IHtmlDocument : IHtmlContainer
+  {
+    /// <summary>
+    /// 获取文档的声明信息，可以是xml声明，也可以是DTD。如果不被支持，则返回null。
+    /// </summary>
+    string DocumentDeclaration
+    {
+      get;
+    }
+
+
+    /// <summary>
+    /// 获取一个节点的句柄或标识
+    /// </summary>
+    /// <param name="node">要获取标识的节点</param>
+    /// <returns>节点的标识</returns>
+    string Handle( IHtmlNode node );
+
+    /// <summary>
+    /// 通过句柄或标识获取节点
+    /// </summary>
+    /// <param name="handler">节点的标识</param>
+    /// <returns>标识的节点</returns>
+    IHtmlNode Handle( string handler );
+
+
+  }
+}

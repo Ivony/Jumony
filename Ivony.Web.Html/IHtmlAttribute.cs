99f88f072a0f37119f56c68ffd1759358fd16d7e
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-08-19 08:04:09 +0000

    dump version:36
    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@59406 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Web.Html/IHtmlAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Web.Html
+{
+  public interface IHtmlAttribute
+  {
+
+    IHtmlElement Element
+    {
+      get;
+    }
+
+    string Name
+    {
+      get;
+    }
+
+    string Value
+    {
+      get;
+      set;
+    }
+
+  }
+}

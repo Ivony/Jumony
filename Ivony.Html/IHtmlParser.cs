962b32cf56903f9d4031361cd14013cfd90a6098
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-11-22 12:52:28 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@64152 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html/IHtmlParser.cs
+++ Ivony.Html/IHtmlParser.cs
@@ -9,7 +9,18 @@ namespace Ivony.Html
   public interface IHtmlParser
   {
 
+    /// <summary>
+    /// 分析 HTML 创建一个文档
+    /// </summary>
+    /// <param name="html">HTML文本</param>
+    /// <returns></returns>
     IHtmlDocument Parse( string html );
+
+
+
+
+
+
 
   }
 }

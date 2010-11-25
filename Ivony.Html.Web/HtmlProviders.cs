3be9532fa4a5f578923a675ebb9ef23df12fb0db
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-11-25 06:29:30 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@64317 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html.Web/HtmlProviders.cs
+++ Ivony.Html.Web/HtmlProviders.cs
@@ -2,23 +2,101 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Ivony.Html.Parser;
 
 namespace Ivony.Html.Web
 {
+
+
+
   public static class HtmlProviders
   {
 
     static HtmlProviders()
     {
-      ParserProviders = new SynchronizedCollection<IHtmlParserProvider>();
+      ParserProviders = new SynchronizedCollection<IHtmlParserProvider>( _parserProvidersSync );
+      HtmlContentProviders = new SynchronizedCollection<IHtmlContentProvider>( _contentProvidersSync );
     }
 
-    
+
+    private static readonly object _parserProvidersSync = new object();
+
     public static ICollection<IHtmlParserProvider> ParserProviders
     {
       get;
       private set;
     }
+
+    private static readonly object _contentProvidersSync = new object();
+
+    public static ICollection<IHtmlContentProvider> HtmlContentProviders
+    {
+      get;
+      private set;
+    }
+
+
+    /// <summary>
+    /// 获取用于分析 HTML 文档的 Parser
+    /// </summary>
+    /// <param name="virtualPath">请求的虚拟路径</param>
+    /// <param name="htmlContent">HTML 文档内容</param>
+    /// <returns>分析后的 HTML 文档</returns>
+    public static IHtmlParser GetParser( string virtualPath, string htmlContent )
+    {
+      lock ( _parserProvidersSync )
+      {
+        foreach ( var provider in ParserProviders )
+        {
+          var parser = provider.GetParser( virtualPath, htmlContent );
+
+          if ( parser != null )
+            return parser;
+        }
+      }
+
+      return new JumonyHtmlParser();
+    }
+
+  
+    /// <summary>
+    /// 加载 HTML 文档内容
+    /// </summary>
+    /// <param name="virtualPath">请求的虚拟路径</param>
+    /// <returns>HTML 文档内容</returns>
+    public static string LoadHtmlContent( string virtualPath )
+    {
+      lock ( _contentProvidersSync )
+      {
+        foreach ( var provider in HtmlContentProviders )
+        {
+          var content = provider.LoadContent( virtualPath );
+
+          if ( content != null )
+            return content;
+        }
+      }
+
+      return null;
+    }
+
+    /// <summary>
+    /// 加载 HTML 文档
+    /// </summary>
+    /// <param name="virtualPath">请求的虚拟路径</param>
+    /// <returns>HTML 文档对象</returns>
+    public static IHtmlDocument LoadDocument( string virtualPath )
+    {
+      var content = LoadHtmlContent( virtualPath );
+      if ( content == null )
+        return null;
+
+      var parser = GetParser( virtualPath, content );
+      
+      return parser.Parse( content );
+    }
+
+
 
   }
 
@@ -33,7 +111,7 @@ namespace Ivony.Html.Web
   public interface IHtmlParserProvider
   {
 
-    IHtmlDocument GetParser( string virtualPath, string htmlContent );
+    IHtmlParser GetParser( string virtualPath, string htmlContent );
 
   }
 

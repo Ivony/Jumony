60638596426a3c930f6d9494c0161815d14a400c
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-12-07 06:21:50 +0000

    Jumony Dynamic
    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@64938 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html.Dynamic/DynamicElement.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+
+namespace Ivony.Html.Dynamic
+{
+  public class DynamicElement : DynamicObject
+  {
+
+    protected IHtmlElement Element
+    {
+      get;
+      private set;
+    }
+
+    public DynamicElement( IHtmlElement element )
+    {
+      Element = element;
+    }
+
+
+    public override bool TryInvokeMember( InvokeMemberBinder binder, object[] args, out object result )
+    {
+      return base.TryInvokeMember( binder, args, out result );
+    }
+
+    public override bool TryGetMember( GetMemberBinder binder, out object result )
+    {
+
+      result = null;
+
+      if ( binder.ReturnType.IsAssignableFrom( typeof( string ) ) )
+      {
+
+        switch ( binder.Name.ToLowerInvariant() )
+        {
+          case "tagName":
+            result = Element.Name;
+            return true;
+        }
+
+        if ( FindAttribute( binder.Name, ref result ) )
+          return true;
+
+      }
+
+      return true;
+
+    }
+
+    private bool FindAttribute( string name, ref object result )
+    {
+      var attribute = Element.Attribute( name );
+      if ( attribute != null )
+      {
+        result = attribute.AttributeValue;
+        return true;
+      }
+
+      return false;
+    }
+
+  }
+}
---
+++ Ivony.Html.Dynamic/DynamicExtension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Web.Html.Dynamic
+{
+  class DynamicExtension
+  {
+  }
+}

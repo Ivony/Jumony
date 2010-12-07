1de154f87039feefe4465b587b57af4ede053dab
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-12-07 01:40:36 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@64924 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html.Web.Mvc/JumonyAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.IO;
+
+namespace Ivony.Html.Web.Mvc
+{
+  public class JumonyAttribute : FilterAttribute, IActionFilter
+  {
+
+    public void OnActionExecuted( ActionExecutedContext filterContext )
+    {
+      var viewResult = filterContext.Result as ViewResultBase;
+
+      if ( viewResult != null )
+        filterContext.Result = new ViewResultWrapper( viewResult );
+    }
+
+    public void OnActionExecuting( ActionExecutingContext filterContext )
+    {
+    }
+
+
+    private class ViewResultWrapper : ActionResult
+    {
+
+      private ViewResultBase _viewResult;
+
+      public ViewResultWrapper( ViewResultBase viewResult )
+      {
+        _viewResult = viewResult;
+      }
+
+
+      public override void ExecuteResult( ControllerContext context )
+      {
+
+      }
+
+    }
+
+  }
+}

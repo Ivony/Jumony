95117a8f11edfdd5be3088f96ee11e9dbdaa44cb
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-12-07 20:03:40 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@64983 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html.Binding/HtmlBindingSheet.cs
+++ Ivony.Html.Binding/HtmlBindingSheet.cs
@@ -87,7 +87,7 @@ namespace Ivony.Html.Binding
 
   public class HtmlBindingRule
   {
-    private Dictionary<string, string> settings = new Dictionary<string, string>( StringComparer.InvariantCultureIgnoreCase );
+    private Dictionary<string, string> settings = new Dictionary<string, string>( StringComparer.OrdinalIgnoreCase );
 
 
     private static readonly Regex styleRulesRegex = new Regex( @"^\s*" + Regulars.styleRulePattern + @"\s*$", RegexOptions.Compiled );

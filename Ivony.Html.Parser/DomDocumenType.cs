a59507fb17caaa83eff6238243ec0f91d591ede6
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-12-07 10:48:10 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@64943 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html.Parser/DomDocumenType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html.Parser
+{
+  public class DomDocumenType : DomNode, IHtmlSpecial
+  {
+
+    private const string DocumentTypePattern = @"\<!DOCTYPE .+?\>";
+
+
+    private string _declares;
+
+    internal DomDocumenType( string declares )
+    {
+      _declares = declares;
+    }
+
+    protected override string ObjectName
+    {
+      get { return "DocumentType Declaration"; }
+    }
+  }
+}

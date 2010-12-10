0dbf087fa98266d4c3b95039efa71ec2ef980892
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-12-10 01:40:38 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@65089 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Core/Fluent/DisposableHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Fluent
+{
+  public static class DisposableHelper
+  {
+    public static IDisposable AsDisposable<T>( this T obj, Action<T> disposeMethod )
+    {
+      if ( obj == null )
+        return null;
+
+      return new DisposableWrapper<T>( obj, disposeMethod );
+    }
+
+    private class DisposableWrapper<T> : IDisposable
+    {
+      private T _obj;
+      private Action<T> _disposableMethod;
+
+      public DisposableWrapper( T obj, Action<T> disposeMethod )
+      {
+        if ( disposeMethod == null )
+          throw new ArgumentNullException( "disposeMethod" );
+
+        _disposableMethod = disposeMethod;
+        _obj = obj;
+      }
+
+      public void Dispose()
+      {
+        _disposableMethod( _obj );
+      }
+    }
+
+  }
+}

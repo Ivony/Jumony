64d27c0970fc2bb0121e9a5c6c38058b5b83de24
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-12-12 01:58:04 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@65190 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html/Properties/AssemblyInfo.cs
+++ Ivony.Html/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle( "HtmlSelector" )]
+[assembly: AssemblyTitle( "Jumony Core" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "HtmlSelector" )]
+[assembly: AssemblyProduct( "" )]
 [assembly: AssemblyCopyright( "Copyright ©  2010" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "3.0.0.*" )]
-[assembly: AssemblyFileVersion( "3.0" )]
+[assembly: AssemblyVersion( "1.1.*" )]
+[assembly: AssemblyFileVersion( "1.1" )]

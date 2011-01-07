3c9ed511fffca57827e4449ba1f1828eabf66d76
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-01-07 06:39:27 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@66563 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html.Web/IHtmlContentProvider.cs
+++ Ivony.Html.Web/IHtmlContentProvider.cs
@@ -10,168 +10,145 @@ using System.Web.Caching;
 namespace Ivony.Html.Web
 {
 
-    public interface IHtmlContentProvider
+  public interface IHtmlContentProvider
+  {
+
+    HtmlContentResult LoadContent( HttpContextBase context, string virtualPath );
+
+  }
+
+  public class HtmlContentResult
+  {
+
+    /// <summary>
+    /// HTML 文本内容
+    /// </summary>
+    public string Content
     {
-
-        HtmlContentResult LoadContent(HttpContextBase context, string virtualPath);
-
-    }
-
-    public class HtmlContentResult
-    {
-
-        /// <summary>
-        /// HTML 文本内容
-        /// </summary>
-        public string Content
-        {
-            get;
-            set;
-        }
-
-
-        /// <summary>
-        /// 获取产生此结果的 HtmlContentProvider
-        /// </summary>
-        public IHtmlContentProvider Provider
-        {
-            get;
-            internal set;
-        }
-
-
-        /// <summary>
-        /// 获取或设置缓存时使用的索引键
-        /// </summary>
-        public string CacheKey
-        {
-            get;
-            set;
-        }
+      get;
+      set;
     }
 
 
     /// <summary>
-    /// 静态文件内容加载器，用于从静态文件中加载HTML内容，会自动缓存
+    /// 获取产生此结果的 HtmlContentProvider
     /// </summary>
-    public class StaticFileLoader : IHtmlContentProvider
+    public IHtmlContentProvider Provider
     {
-
-        protected static Cache Cache
-        {
-            get { return HostingEnvironment.Cache; }
-        }
-
-
-        private static readonly string[] allowsExtensions = new[] { ".html", ".htm" };
-
-        public HtmlContentResult LoadContent(HttpContextBase context, string virtualPath)
-        {
-
-            if (context == null)
-                throw new ArgumentNullException("context");
-
-
-            if (!allowsExtensions.Contains(VirtualPathUtility.GetExtension(virtualPath)))
-                return null;
-
-            return LoadContent(HostingEnvironment.VirtualPathProvider, virtualPath);
-        }
-
-
-        private static HtmlContentResult LoadContent(VirtualPathProvider provider, string virtualPath)
-        {
-
-            if (!provider.FileExists(virtualPath))
-                return null;
-
-            var file = provider.GetFile(virtualPath);
-
-            if (file == null)
-                return null;
-
-
-            var key = string.Format("StaticFile_{0}",  virtualPath);
-
-            var fileInfo = Cache.Get(key) as VirtualFileInfo;
-
-
-
-            if (fileInfo == null || fileInfo.VirtualPath != virtualPath)
-            {
-                fileInfo = new VirtualFileInfo() { VirtualPath = virtualPath, Content = LoadContent(file) };
-
-                fileInfo.CacheDependency = provider.GetCacheDependency(virtualPath, null, DateTime.UtcNow);
-
-                Cache.Insert(key, fileInfo, fileInfo.CacheDependency);
-            }
-
-
-            return new HtmlContentResult()
-            {
-                Content = fileInfo.Content,
-                CacheKey = key,
-            };
-        }
-
-        protected static string LoadContent(VirtualFile file)
-        {
-            using (var reader = new StreamReader(file.Open(), true))
-            {
-                return reader.ReadToEnd();
-            }
-        }
-
-        protected class VirtualFileInfo
-        {
-            public string VirtualPath
-            {
-                get;
-                set;
-            }
-
-            public string Content
-            {
-                get;
-                set;
-            }
-
-            public CacheDependency CacheDependency
-            {
-                get;
-                set;
-            }
-        }
-
+      get;
+      internal set;
     }
 
 
-    public class AspxFileLoader : IHtmlContentProvider
+    /// <summary>
+    /// 获取或设置缓存时使用的索引键
+    /// </summary>
+    public string CacheKey
+    {
+      get;
+      set;
+    }
+  }
+
+
+  /// <summary>
+  /// 静态文件内容加载器，用于从静态文件中加载HTML内容，会自动缓存
+  /// </summary>
+  public class StaticFileLoader : IHtmlContentProvider
+  {
+
+    protected static Cache Cache
+    {
+      get { return HostingEnvironment.Cache; }
+    }
+
+
+    private static readonly string[] allowsExtensions = new[] { ".html", ".htm" };
+
+    public HtmlContentResult LoadContent( HttpContextBase context, string virtualPath )
     {
 
-        private static readonly string[] allowsExtensions = new[] { ".aspx" };
-
-        public HtmlContentResult LoadContent(HttpContextBase context, string virtualPath)
-        {
-
-            if (context == null)
-                throw new ArgumentNullException("context");
+      if ( context == null )
+        throw new ArgumentNullException( "context" );
 
 
-            if (!allowsExtensions.Contains(VirtualPathUtility.GetExtension(virtualPath)))
-                return null;
+      if ( !allowsExtensions.Contains( VirtualPathUtility.GetExtension( virtualPath ) ) )
+        return null;
 
-
-            using (var writer = new StringWriter())
-            {
-                context.Server.Execute(virtualPath, writer, false);
-
-                return new HtmlContentResult()
-                {
-                    Content = writer.ToString()
-                };
-            }
-        }
+      return LoadContent( HostingEnvironment.VirtualPathProvider, virtualPath );
     }
+
+
+    public static HtmlContentResult LoadContent( VirtualPathProvider provider, string virtualPath )
+    {
+
+      if ( !provider.FileExists( virtualPath ) )
+        return null;
+
+      var file = provider.GetFile( virtualPath );
+
+      if ( file == null )
+        return null;
+
+
+      var key = string.Format( "StaticFile_{1}_{0}", virtualPath, provider.Equals( HostingEnvironment.VirtualPathProvider ) ? null : provider );
+
+
+      var content = Cache.Get( key ) as string;
+
+
+      if ( content == null )
+      {
+
+        content = LoadContent( file );
+
+        Cache.Insert( key, content, provider.GetCacheDependency( virtualPath, null, DateTime.UtcNow ) );
+      }
+
+
+      return new HtmlContentResult()
+      {
+        Content = content,
+        CacheKey = key,
+      };
+    }
+
+    protected static string LoadContent( VirtualFile file )
+    {
+      using ( var reader = new StreamReader( file.Open(), true ) )
+      {
+        return reader.ReadToEnd();
+      }
+    }
+  }
+
+
+  public class AspxFileLoader : IHtmlContentProvider
+  {
+
+    private static readonly string[] allowsExtensions = new[] { ".aspx" };
+
+    public HtmlContentResult LoadContent( HttpContextBase context, string virtualPath )
+    {
+
+      if ( context == null )
+        throw new ArgumentNullException( "context" );
+
+
+      if ( !allowsExtensions.Contains( VirtualPathUtility.GetExtension( virtualPath ) ) )
+        return null;
+
+
+      using ( var writer = new StringWriter() )
+      {
+        context.Server.Execute( virtualPath, writer, false );
+
+        return new HtmlContentResult()
+        {
+          Content = writer.ToString()
+        };
+      }
+    }
+  }
 
 }

9fffa114e91816b2bb64cb81753f100c78286c60
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-01-23 04:41:06 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@67272 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Core/PostBuild.bat
+++
@@ -1,7 +0,0 @@
-cd %ProjectDir%
-"c:\Program Files\CollabNet\Subversion Client\SvnCommitHelper.exe"
-
-DEL "%TargetDir%Code.RAR"
-"%ProgramFiles%\WinRAR\RAR.exe" a -cfg- -inul -k -ep1 -m5 -rr -x"%TargetDir%" -x*.pfx -x*.dll -x*.pdb -x*.exe -x.svn "%TargetDir%Code.RAR" "%ProjectDir%..\%ProjectName%"
-"%ProgramFiles%\WinRAR\RAR.exe" a -cfg- -inul -k -ep1 -m5 -rr -ag_yyyy.mm.dd_n -ms*.rar -x*.tmp -x*.pdb -x*.pfx -x.svn C:\Assembly\%ProjectName%_%ConfigurationName%.RAR "%TargetDir%*.*"
-DEL "%TargetDir%Code.RAR"

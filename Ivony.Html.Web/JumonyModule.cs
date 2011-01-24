fdea884cb757ee2d16d12f3179ab880da7577188
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-01-24 07:44:42 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@67315 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html.Web/JumonyModule.cs
+++ Ivony.Html.Web/JumonyModule.cs
@@ -17,43 +17,10 @@ namespace Ivony.Html.Web
 
     public void Init( HttpApplication context )
     {
-      context.ResolveRequestCache += new EventHandler( OnResolveRequestCache );
       context.PostResolveRequestCache += new EventHandler( OnPreMapRequestHandler );
-
-      _application = context;
     }
 
-    private void OnResolveRequestCache( object sender, EventArgs e )
-    {
-
-      var context = HttpContext.Current;
-
-      var key = HtmlProviders.GetCacheKey( new HttpContextWrapper( context ) );
-
-      if ( key != null )
-      {
-
-        var outputCache = context.Cache.Get( key ) as string;
-
-        if ( outputCache != null )
-        {
-          context.Response.Write( outputCache );
-
-          context.Response.End();
-        }
-
-      }
-
-    }
-
-
-    private HttpApplication _application;
-
-
-
-
-
-    private void OnPreMapRequestHandler( object sender, EventArgs e )
+    void OnPreMapRequestHandler( object sender, EventArgs e )
     {
       var context = HttpContext.Current;
 

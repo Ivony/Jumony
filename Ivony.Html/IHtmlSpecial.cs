91befe063f862854533552fd25b082b03ea45f18
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-02-25 16:57:35 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@69405 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html/IHtmlSpecial.cs
+++ Ivony.Html/IHtmlSpecial.cs
@@ -10,7 +10,8 @@ namespace Ivony.Html
   /// 定义 HTML 特殊节点，其不是 HTML 的组成部分，例如代码块
   /// </summary>
   /// <remarks>
-  /// 若此节点最终能生成 HTML 内容，应同时实现 IHtmlRenderable 接口
+  /// 若此节点最终能生成 HTML 内容，应同时实现 IHtmlRenderable 接口。
+  /// 特殊节点不被视为HTML的一部分，不认为会产生任何文本，也不自动生成HTML，而使用RawHtml原样输出
   /// </remarks>
   public interface IHtmlSpecial : IHtmlNode
   {

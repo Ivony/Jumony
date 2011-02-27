af9cf88770cc2b7b084be630c3015b233fc48652
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-02-27 16:19:09 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@69481 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html.Binding/IHtmlContent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html.Binding
+{
+  public interface IHtmlContent
+  {
+
+    string ToHtmlString();
+
+  }
+}

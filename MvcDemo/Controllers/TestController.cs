efac944cafdf731b7af222088b8af8ce53647d24
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-03-08 03:21:22 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@69959 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ MvcDemo/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

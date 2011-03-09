bb06fabb82b265f20f2c80416dd7a9d16f8d1ac1
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-03-09 16:23:14 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@70061 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html.Web.Mvc/JumonyViewResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Ivony.Html.Web.Mvc
+{
+  public class JumonyViewResult : ActionResult
+  {
+    public override void ExecuteResult( ControllerContext context )
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

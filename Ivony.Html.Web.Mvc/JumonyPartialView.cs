b6118c9e8eecf3b8dd5838a658ec53396f0448f6
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-03-17 05:48:28 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@70490 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html.Web.Mvc/JumonyPartialView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html.Web.Mvc
+{
+  public class JumonyPartialView : JumonyViewBase
+  {
+
+    protected IHtmlContainer ViewContainer
+    {
+      get;
+      private set;
+    }
+
+    public void Initialize( string virtualPath, IHtmlDocument document )
+    {
+      ViewContainer = document.FindSingle( "body" );
+    }
+    
+    protected override void ProcessMain()
+    {
+      throw new NotImplementedException();
+    }
+
+    protected override string RenderContent()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

1e479bc679ac98d643f7fb1e0aa56bab61319f18
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-03-28 15:41:45 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@71038 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html.Web.Mvc/SimpleRoutingTable.cs
+++ Ivony.Html.Web.Mvc/SimpleRoutingTable.cs
@@ -6,6 +6,7 @@ using System.Web.Routing;
 using System.Text.RegularExpressions;
 using System.Web;
 using Ivony.Fluent;
+using System.Collections.Specialized;
 
 namespace Ivony.Html.Web.Mvc
 {
@@ -13,7 +14,32 @@ namespace Ivony.Html.Web.Mvc
   {
     public override RouteData GetRouteData( HttpContextBase httpContext )
     {
-      throw new NotImplementedException();
+      var virtualPath = httpContext.Request.AppRelativeCurrentExecutionFilePath;
+
+      var data = _rules
+        .OrderBy( r => r.DynamicRouteKyes.Length )
+        .Select( r => new
+          {
+            Rule = r,
+            Values = r.GetRouteValues( virtualPath, httpContext.Request.QueryString ),
+          } )
+        .NotNull().FirstOrDefault();
+
+      if ( data == null )
+        return null;
+
+      var handler = data.Rule.CreateHandler( httpContext );
+
+      var routeData = new RouteData( this, handler );
+
+      foreach ( var pair in data.Values )
+        routeData.Values.Add( pair.Key, pair.Value );
+
+
+      routeData.DataTokens["RoutingRuleName"] = data.Rule.Name;
+
+      return routeData;
+
     }
 
     public override VirtualPathData GetVirtualPath( RequestContext requestContext, RouteValueDictionary values )
@@ -33,7 +59,7 @@ namespace Ivony.Html.Web.Mvc
         return null;
 
 
-      var bestRule = GetBestRule( candidateRules );
+      var bestRule = BestRule( candidateRules );
 
       var virtualPath = bestRule.CreateVirtualPath( values );
 
@@ -41,9 +67,9 @@ namespace Ivony.Html.Web.Mvc
     }
 
 
-    public void AddRule( string urlPattern, IDictionary<string, string> routeValues, string[] queryKeys )
+    public void AddRule( string name, string urlPattern, IDictionary<string, string> routeValues, string[] queryKeys )
     {
-      var rule = new SimpleRoutingRule( urlPattern, routeValues, queryKeys );
+      var rule = new SimpleRoutingRule( name, urlPattern, routeValues, queryKeys );
 
       AddRule( rule );
     }
@@ -54,8 +80,16 @@ namespace Ivony.Html.Web.Mvc
       var candidateRules = _rules
         .Where( r => r.AllKeys.Length == rule.AllKeys.Length )                        //若通过所有键多寡无法区分
         .Where( r => r.RouteKeys.Length == rule.RouteKeys.Length )                    //若通过RouteKey多寡无法区分
-        .Where( r => r.DynamicParagraphes.Length == rule.DynamicParagraphes.Length )  //若通过动态路径段多寡也无法区分
+        .Where( r => r.DynamicRouteKyes.Length == rule.DynamicRouteKyes.Length )  //若通过动态路径段多寡也无法区分
         .Where( r => r.EqualsConstraints( rule ) );                                   //若约束集也一致
+
+      if ( candidateRules.Any() )
+        throw new InvalidOperationException( "添加规则失败，路由表中已经存在一条可能冲突的规则" );
+
+
+      candidateRules = _rules
+        .Where( r => r.Paragraphes.Length == rule.Paragraphes.Length )
+        .Where( r => r.UrlPattern.Substring( 0, r.UrlPattern.IndexOf( '{' ) ) == rule.UrlPattern.Substring( 0, r.UrlPattern.IndexOf( '{' ) ) );
 
       if ( candidateRules.Any() )
         throw new InvalidOperationException( "添加规则失败，路由表中已经存在一条可能冲突的规则" );
@@ -63,7 +97,7 @@ namespace Ivony.Html.Web.Mvc
     }
 
 
-    private SimpleRoutingRule GetBestRule( IEnumerable<SimpleRoutingRule> candidateRules )
+    private SimpleRoutingRule BestRule( IEnumerable<SimpleRoutingRule> candidateRules )
     {
 
       var maxConstraints = candidateRules.Max( r => r.RouteValueConstraints.Count );            //满足最多约束的被优先考虑
@@ -87,8 +121,8 @@ namespace Ivony.Html.Web.Mvc
         return candidateRules.Single();
 
 
-      var minDynamics = candidateRules.Min( p => p.DynamicParagraphes.Length );                 //拥有最少动态参数的被优先考虑
-      candidateRules = candidateRules.Where( r => r.DynamicParagraphes.Length == minDynamics );
+      var minDynamics = candidateRules.Min( p => p.DynamicRouteKyes.Length );                 //拥有最少动态参数的被优先考虑
+      candidateRules = candidateRules.Where( r => r.DynamicRouteKyes.Length == minDynamics );
 
       if ( candidateRules.IsSingle() )
         return candidateRules.Single();
@@ -97,6 +131,7 @@ namespace Ivony.Html.Web.Mvc
         return null;
 
     }
+
 
 
 
@@ -124,8 +159,10 @@ namespace Ivony.Html.Web.Mvc
 
     private static readonly Regex urlPatternRegex = new Regex( urlPattern, RegexOptions.Compiled );
 
-    public SimpleRoutingRule( string urlPattern, IDictionary<string, string> routeValues, string[] queryKeys )
+    public SimpleRoutingRule( string name, string urlPattern, IDictionary<string, string> routeValues, string[] queryKeys )
     {
+
+      Name = name;
 
       var match = urlPatternRegex.Match( urlPattern );
 
@@ -135,9 +172,9 @@ namespace Ivony.Html.Web.Mvc
       _paragraphes = match.Groups["paragraph"].Captures.Cast<Capture>().Select( c => c.Value ).ToArray();
 
       _urlPattern = urlPattern;
-      _routeValues = routeValues.ToDictionary( pair => pair.Key, pair => pair.Value );
+      _routeValuesConstraints = routeValues.ToDictionary( pair => pair.Key, pair => pair.Value );
 
-      var keys = new HashSet<string>( _routeValues.Keys );
+      var keys = new HashSet<string>( _routeValuesConstraints.Keys );
 
       var _dynamicParagraphes = _paragraphes.Where( p => p.StartsWith( "{" ) && p.EndsWith( "}" ) ).ToArray();
 
@@ -159,6 +196,14 @@ namespace Ivony.Html.Web.Mvc
       _queryKeys = queryKeys;
 
     }
+
+
+    public string Name
+    {
+      get;
+      private set;
+    }
+
 
 
     private string[] _paragraphes;
@@ -200,22 +245,41 @@ namespace Ivony.Html.Web.Mvc
     }
 
 
-    private string[] _dynamicParagraphes;
+    private string[] _dynamics;
 
-    public string[] DynamicParagraphes
+    public string[] DynamicRouteKyes
     {
-      get { return _dynamicParagraphes; }
+      get { return _dynamics; }
     }
 
+
+    private string _prefix;
+
+    public string StaticPrefix
+    {
+      get
+      {
+        if ( _prefix == null )
+          _prefix = _urlPattern.Substring( 0, _urlPattern.IndexOf( '{' ) );
+
+        return _prefix;
+      }
+    }
 
 
     private string _urlPattern;
 
-    private IDictionary<string, string> _routeValues;
+    public string UrlPattern
+    {
+      get { return _urlPattern; }
+    }
+
+
+    private IDictionary<string, string> _routeValuesConstraints;
 
     public IDictionary<string, string> RouteValueConstraints
     {
-      get { return new Dictionary<string, string>( _routeValues ); }
+      get { return new Dictionary<string, string>( _routeValuesConstraints ); }
     }
 
 
@@ -229,14 +293,19 @@ namespace Ivony.Html.Web.Mvc
 
     public bool IsMatch( IDictionary<string, string> values )
     {
-      foreach ( var key in _routeValues.Keys )
+
+      if ( values == null )
+        throw new ArgumentNullException( "values" );
+
+
+      foreach ( var key in _routeValuesConstraints.Keys )
       {
         string value;
 
         if ( values.TryGetValue( key, out value ) )
           return false;
 
-        if ( _routeValues[key] != value )
+        if ( _routeValuesConstraints[key] != value )
           return false;
       }
 
@@ -246,10 +315,77 @@ namespace Ivony.Html.Web.Mvc
     public bool EqualsConstraints( SimpleRoutingRule rule )
     {
 
+      if ( rule == null )
+        throw new ArgumentNullException( "rule" );
+
+
       if ( rule.RouteValueConstraints.Count != RouteValueConstraints.Count )
         return false;
 
       return IsMatch( rule.RouteValueConstraints );
+
+    }
+
+    public virtual IDictionary<string, string> GetRouteValues( string virtualPath, NameValueCollection queryString )
+    {
+
+      if ( virtualPath == null )
+        throw new ArgumentNullException( "virtualPath" );
+
+      if ( !VirtualPathUtility.IsAppRelative( virtualPath ) )
+        throw new ArgumentException( "必须是相对于应用程序根的路径" );
+
+      var queryKeySet = new HashSet<string>( _queryKeys );
+
+      if ( !queryKeySet.IsSupersetOf( queryString.AllKeys ) )
+        return null;
+
+
+      var pathParagraphs = virtualPath.Substring( 2 ).Split( '/' );
+
+      if ( pathParagraphs.Length != Paragraphes.Length )
+        return null;
+
+
+
+      var values = new Dictionary<string, string>();
+
+
+      foreach ( var key in queryString.AllKeys )
+        values[key] = queryString[key];
+
+
+
+      for ( int i = 0; i < pathParagraphs.Length; i++ )
+      {
+
+        var paragraph = Paragraphes[i];
+
+        if ( !paragraph.StartsWith( "{" ) )
+        {
+          if ( pathParagraphs[i] != paragraph )
+            return null;
+        }
+        else
+        {
+          var name = paragraph.Substring( 1, paragraph.Length - 2 );
+          values[name] = pathParagraphs[i];
+        }
+      }
+
+
+      foreach ( var pair in _routeValuesConstraints )
+        values[pair.Key] = pair.Value;
+
+
+
+      return values;
+    }
+
+    public virtual IRouteHandler CreateHandler( HttpContextBase httpContext )
+    {
+
+      throw new NotSupportedException();
 
     }
   }

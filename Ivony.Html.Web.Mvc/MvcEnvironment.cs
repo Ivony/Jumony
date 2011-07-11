9abf686ee662523b68da2cb8e7559dcc94fe32a7
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-07-11 11:16:42 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@76760 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html.Web.Mvc/MvcEnvironment.cs
+++ Ivony.Html.Web.Mvc/MvcEnvironment.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 [assembly: PreApplicationStartMethod( typeof( Ivony.Html.Web.Mvc.MvcEnvironment ), "Initialize" )]
 
@@ -12,18 +13,38 @@ namespace Ivony.Html.Web.Mvc
 
   public static class MvcEnvironment
   {
-
+    /// <summary>
+    /// 此方法由 ASP.NET 4 系统调用，不应从用户代码中直接调用。
+    /// </summary>
     public static void Initialize()
     {
       ViewEngines.Engines.Add( _viewEngine );
+      RouteTable.Routes.Add( _simpleRoutingTable );
     }
+
+
 
     private static JumonyViewEngine _viewEngine = new JumonyViewEngine();
 
+    /// <summary>
+    /// 获取 Jumony 视图引擎的默认实例
+    /// </summary>
     public static JumonyViewEngine JumonyViewEngine
     {
       get { return _viewEngine; }
     }
+
+
+    private static SimpleRoutingTable _simpleRoutingTable = new SimpleRoutingTable( new MvcRouteHandler(), true );
+
+    /// <summary>
+    /// 获取简单路由表的默认实例
+    /// </summary>
+    public static SimpleRoutingTable SimpleRoutingTable
+    {
+      get { return _simpleRoutingTable; }
+    }
+
 
 
 
---
+++ Ivony.Html.Web.Mvc/RoutingExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Routing;
+
+namespace Ivony.Html.Web.Mvc
+{
+  public static class RoutingExtensions
+  {
+
+    public static SimpleRoutingTable MapAction( this SimpleRoutingTable routingTable, string urlPattern, string controller, string action )
+    {
+      return MapAction( routingTable, string.Format( "{0}/{1}", controller, action ), urlPattern, controller, action );
+    }
+
+
+    public static SimpleRoutingTable MapAction( this SimpleRoutingTable routingTable, string name, string urlPattern, string controller, string action )
+    {
+      return MapAction( routingTable, name, urlPattern, controller, action, new string[0] );
+    }
+
+
+    public static SimpleRoutingTable MapAction( this SimpleRoutingTable routingTable, string name, string urlPattern, string controller, string action, string[] queryKeys )
+    {
+      routingTable.AddRule( name, urlPattern, new Dictionary<string, string>() { { "action", action }, { "controller", controller } }, queryKeys );
+
+      return routingTable;
+    }
+
+
+  }
+}

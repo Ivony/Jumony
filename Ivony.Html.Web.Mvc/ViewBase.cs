511528c571e2833813319509656711fff4e3222c
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-08-01 03:28:19 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@77624 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html.Web.Mvc/ViewBase.cs
+++ Ivony.Html.Web.Mvc/ViewBase.cs
@@ -144,9 +144,14 @@ namespace Ivony.Html.Web.Mvc
 
       Url = new UrlHelper( RequestContext );
 
+      HttpContext.Trace.Write( "Begin Process" );
       ProcessMain();
+      HttpContext.Trace.Write( "End Process" );
 
+
+      HttpContext.Trace.Write( "Begin Render" );
       var content = RenderContent();
+      HttpContext.Trace.Write( "End Render" );
 
       UpdateCache( content );
 
@@ -424,11 +429,11 @@ namespace Ivony.Html.Web.Mvc
         var partialTag = ContentExtensions.GenerateTagHtml( element, true );
 
 
-        _view.HttpContext.Trace.Write( "Begin Render Partial: {0}", partialTag );
+        _view.HttpContext.Trace.Write( "Begin Render Partial", partialTag );
 
         _view.RenderPartial( element, writer );
 
-        _view.HttpContext.Trace.Write( "End Render Partial: {0}", partialTag );
+        _view.HttpContext.Trace.Write( "End Render Partial", partialTag );
       }
     }
 

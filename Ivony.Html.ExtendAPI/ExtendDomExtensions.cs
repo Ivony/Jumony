c8d65be35ceef496227257786c9fd75df2b23963
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-08-03 05:41:35 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@77702 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html.ExtendAPI/ExtendDomExtensions.cs
+++ Ivony.Html.ExtendAPI/ExtendDomExtensions.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Ivony.Html.ExtendDom
 {
-  public static class DomExtensions
+  public static class ExtendDomExtensions
   {
 
 
@@ -385,6 +385,106 @@ namespace Ivony.Html.ExtendDom
       lock ( container.SyncRoot )
       {
         return container.AddCopy( node.NodesIndexOfSelf(), comment );
+      }
+    }
+
+
+
+    /// <summary>
+    /// 在后面添加节点的副本
+    /// </summary>
+    /// <param name="node">要在其后面添加副本的节点</param>
+    /// <param name="element">要创作副本的节点</param>
+    /// <returns>添加后的节点</returns>
+    public static IHtmlComment AddCopyAfterSelf( this IHtmlNode node, IHtmlElement element )
+    {
+      if ( node == null )
+        throw new ArgumentNullException( "node" );
+
+      if ( element == null )
+        throw new ArgumentNullException( "comment" );
+
+
+      var container = node.Container;
+
+      lock ( container.SyncRoot )
+      {
+        return container.AddCopy( node.NodesIndexOfSelf() + 1, element );
+      }
+    }
+
+
+
+    /// <summary>
+    /// 在前面添加节点的副本
+    /// </summary>
+    /// <param name="node">要在其前面添加副本的节点</param>
+    /// <param name="element">要创作副本的节点</param>
+    /// <returns>添加后的节点</returns>
+    public static IHtmlComment AddCopyBeforeSelf( this IHtmlNode node, IHtmlElement element )
+    {
+      if ( node == null )
+        throw new ArgumentNullException( "node" );
+
+      if ( element == null )
+        throw new ArgumentNullException( "comment" );
+
+
+      var container = node.Container;
+
+      lock ( container.SyncRoot )
+      {
+        return container.AddCopy( node.NodesIndexOfSelf(), element );
+      }
+    }
+
+
+
+    /// <summary>
+    /// 在后面添加节点的副本
+    /// </summary>
+    /// <param name="node">要在其后面添加副本的节点</param>
+    /// <param name="newNode">要创作副本的节点</param>
+    /// <returns>添加后的节点</returns>
+    public static IHtmlComment AddCopyAfterSelf( this IHtmlNode node, IHtmlNode newNode )
+    {
+      if ( node == null )
+        throw new ArgumentNullException( "node" );
+
+      if ( newNode == null )
+        throw new ArgumentNullException( "comment" );
+
+
+      var container = node.Container;
+
+      lock ( container.SyncRoot )
+      {
+        return container.AddCopy( node.NodesIndexOfSelf() + 1, newNode );
+      }
+    }
+
+
+
+    /// <summary>
+    /// 在前面添加节点的副本
+    /// </summary>
+    /// <param name="node">要在其前面添加副本的节点</param>
+    /// <param name="newNode">要创作副本的节点</param>
+    /// <returns>添加后的节点</returns>
+    public static IHtmlComment AddCopyBeforeSelf( this IHtmlNode node, IHtmlNode newNode )
+    {
+      if ( node == null )
+        throw new ArgumentNullException( "node" );
+
+      if ( newNode == null )
+        throw new ArgumentNullException( "comment" );
+
+
+      var container = node.Container;
+
+      lock ( container.SyncRoot )
+      {
+        return container.AddCopy( node.NodesIndexOfSelf(), newNode );
       }
     }
 

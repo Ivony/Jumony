3d68653ddc732f7dda8b957da23dc475b728b06c
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-08-03 08:36:37 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@77710 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html/Extensions/ExpandedDomExtensions.cs
+++ Ivony.Html/Extensions/ExpandedDomExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Ivony.Html.ExtendDomAPI
+namespace Ivony.Html.ExpandedDomAPI
 {
   public static class ExtendDomExtensions
   {

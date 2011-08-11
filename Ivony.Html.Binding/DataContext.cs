092e59ba83545fc3612a6958b2fddf54a1afc6e5
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-08-11 08:58:53 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@78126 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html.Binding/DataContext.cs
+++ Ivony.Html.Binding/DataContext.cs
@@ -2,14 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 namespace Ivony.Html.Binding
 {
   public class DataContext
   {
 
+    private BindingContext _bindingContext;
+
+    public DataContext( BindingContext bindingContext )
+    {
+      _bindingContext = bindingContext;
+    }
 
 
+    private Hashtable _dataItems = new Hashtable();
+
+
+    public object GetData( IHtmlNode node )
+    {
+
+      return GetData( node, true );
+
+    }
+
+    protected object GetData( IHtmlNode node, bool asContainer )
+    {
+
+      if ( node == null )
+        return null;
+
+
+      if ( asContainer )
+      {
+        var _container = node as IHtmlContainer;
+
+        if ( _container != null && _dataItems.Contains( _container ) )
+          return _dataItems[_container];
+      }
+
+
+      var container = node.Container;
+
+      if ( _dataItems.Contains( container ) )
+        return _dataItems[container];
+
+      else
+        return GetData( container as IHtmlNode, false );
+    }
 
 
 

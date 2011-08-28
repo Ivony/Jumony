cbc8799328054b6d28479f58df89ed29dbc13997
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-08-28 07:19:41 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@78990 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html.Web.Mvc/ScriptManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html.Web.Mvc
+{
+  
+  /// <summary>
+  /// 注册和管理脚本引用
+  /// </summary>
+  public class ScriptManager
+  {
+
+
+
+
+  }
+}

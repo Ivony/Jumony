c2d7fd813498abb5dd604707a20a3dee91461cb6
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-08-28 13:21:18 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@79008 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html/Selectors/CssSelector.cs
+++ Ivony.Html/Selectors/CssSelector.cs
@@ -49,17 +49,13 @@ namespace Ivony.Html
       if ( expression == null )
         throw new ArgumentNullException( "expression" );
 
-      Trace.TraceInformation( "Begin match expression" );
       var match = cssSelectorRegex.Match( expression );
-      Trace.TraceInformation( "End match expression" );
 
       if ( !match.Success )
         throw new FormatException( "无法识别的CSS选择器" );
 
 
-      Trace.TraceInformation( "Begin create casecading selectors" );
       var selectors = match.Groups["selector"].Captures.Cast<Capture>().Select( c => CssCasecadingSelector.Create( c.Value, scope ) ).ToArray();
-      Trace.TraceInformation( "End create casecading selectors" );
 
       return new CssMultipleSelector( selectors );
     }
@@ -84,9 +80,7 @@ namespace Ivony.Html
 
       try
       {
-        Trace.TraceInformation( "Create Selector" );
         var selector = Create( expression, scope );
-        Trace.TraceInformation( "Create Selector completed" );
         return selector.Filter( scope.Descendants() );
       }
       catch ( Exception e )

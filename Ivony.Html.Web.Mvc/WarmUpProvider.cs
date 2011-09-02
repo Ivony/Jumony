f9a0c96b0d08110040860ec8c4cfcb22f33c7bdc
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-09-02 16:17:11 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@79322 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html.Web.Mvc/WarmUpProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Hosting;
+
+namespace Ivony.Html.Web.Mvc
+{
+  public class WarmUpProvider : IProcessHostPreloadClient
+  {
+    public void Preload( string[] parameters )
+    {
+      MvcEnvironment.WarmUp();
+    }
+  }
+}

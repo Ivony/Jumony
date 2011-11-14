0b0917246b4e46cb887f6db20986de93a28da7b2
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-11-14 16:09:31 +0000

    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@82859 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html/DomDependency.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html
+{
+  public class DomDependency : IDisposable
+  {
+
+    internal DomDependency( IHtmlContainer container )
+    {
+      Container = container;
+
+      AddDomChangedEventHandler( container );
+
+    }
+
+    private void AddDomChangedEventHandler( IHtmlContainer container )
+    {
+      EventRaiser = container as INotifyDomChanged;
+      if ( EventRaiser != null )
+      {
+        EventRaiser.HtmlDomChanged += DomChanged;
+        return;
+      }
+
+      EventRaiser = container.Document as INotifyDomChanged;
+      if ( EventRaiser != null )
+      {
+        EventRaiser.HtmlDomChanged += DomChanged;
+        return;
+      }
+
+
+      EventRaiser = container.Document.DomModifier as INotifyDomChanged;
+      if ( EventRaiser != null )
+      {
+        EventRaiser.HtmlDomChanged += DomChanged;
+        return;
+      }
+
+      throw new NotSupportedException();
+    }
+
+
+
+    private INotifyDomChanged EventRaiser
+    {
+      get;
+      set;
+    }
+
+    private void DomChanged( object sender, HtmlDomChangedEventArgs e )
+    {
+      HasChanged = true;
+      EventRaiser.HtmlDomChanged -= DomChanged;
+    }
+
+
+    public IHtmlContainer Container
+    {
+      get;
+      private set;
+    }
+
+    public bool HasChanged
+    {
+      get;
+      private set;
+    }
+
+
+
+    public void Dispose()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

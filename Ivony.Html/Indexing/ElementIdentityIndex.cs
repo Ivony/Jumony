88de5debc42a6debc2ee2edbc412a8a280329a72
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2012-01-02 13:26:49 +0000

    修正和优化文档索引
    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@84660 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html/Indexing/ElementIdentityIndex.cs
+++ Ivony.Html/Indexing/ElementIdentityIndex.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Ivony.Fluent;
 
 namespace Ivony.Html.Indexing
 {
@@ -17,36 +18,36 @@ namespace Ivony.Html.Indexing
   /// <summary>
   /// 文档的 ID 索引
   /// </summary>
-  public class ElementIdentityIndex : IDictionary<string, IHtmlElement>
+  public class ElementIdentityIndex
   {
+
+
+    private IHtmlDocument _document;
+
     internal ElementIdentityIndex( IHtmlDocument document )
     {
-
+      _document = document;
     }
 
 
     private Dictionary<string, IHtmlElement> data = new Dictionary<string, IHtmlElement>();
 
 
-
-    /// <summary>
-    /// 索引编写器
-    /// </summary>
-    public class Indexer : IElementIndexer
+    public void Rebuild()
     {
+      data = new Dictionary<string, IHtmlElement>();
 
-      private ElementIdentityIndex _index;
-
-      private Indexer( ElementIdentityIndex index )
-      {
-        _index = index;
-      }
-
-      void IElementIndexer.IndexElement( IHtmlElement element )
-      {
-        _index.IndexElement( element );
-      }
+      _document.Descendants().ForAll( element => IndexElement( element ) );
     }
+
+
+    public IHtmlElement this[string id]
+    {
+      get { return data[id]; }
+    }
+
+
+
 
 
 
@@ -77,85 +78,5 @@ namespace Ivony.Html.Indexing
 
 
 
-    void IDictionary<string, IHtmlElement>.Add( string key, IHtmlElement value )
-    {
-      throw new NotSupportedException();
-    }
-
-    bool IDictionary<string, IHtmlElement>.ContainsKey( string key )
-    {
-      return data.ContainsKey( key );
-    }
-
-    ICollection<string> IDictionary<string, IHtmlElement>.Keys
-    {
-      get { return data.Keys.ToArray(); }
-    }
-
-    bool IDictionary<string, IHtmlElement>.Remove( string key )
-    {
-      throw new NotSupportedException();
-    }
-
-    bool IDictionary<string, IHtmlElement>.TryGetValue( string key, out IHtmlElement value )
-    {
-      return data.TryGetValue( key, out value );
-    }
-
-    ICollection<IHtmlElement> IDictionary<string, IHtmlElement>.Values
-    {
-      get { return data.Values.ToArray(); }
-    }
-
-    IHtmlElement IDictionary<string, IHtmlElement>.this[string key]
-    {
-      get { return data[key]; }
-      set { throw new NotSupportedException(); }
-    }
-
-    void ICollection<KeyValuePair<string, IHtmlElement>>.Add( KeyValuePair<string, IHtmlElement> item )
-    {
-      throw new NotSupportedException();
-    }
-
-    void ICollection<KeyValuePair<string, IHtmlElement>>.Clear()
-    {
-      throw new NotSupportedException();
-    }
-
-    bool ICollection<KeyValuePair<string, IHtmlElement>>.Contains( KeyValuePair<string, IHtmlElement> item )
-    {
-      return data.Contains( item );
-    }
-
-    void ICollection<KeyValuePair<string, IHtmlElement>>.CopyTo( KeyValuePair<string, IHtmlElement>[] array, int arrayIndex )
-    {
-      throw new NotSupportedException();
-    }
-
-    int ICollection<KeyValuePair<string, IHtmlElement>>.Count
-    {
-      get { return data.Count; }
-    }
-
-    bool ICollection<KeyValuePair<string, IHtmlElement>>.IsReadOnly
-    {
-      get { return true; }
-    }
-
-    bool ICollection<KeyValuePair<string, IHtmlElement>>.Remove( KeyValuePair<string, IHtmlElement> item )
-    {
-      throw new NotSupportedException();
-    }
-
-    IEnumerator<KeyValuePair<string, IHtmlElement>> IEnumerable<KeyValuePair<string, IHtmlElement>>.GetEnumerator()
-    {
-      return data.GetEnumerator();
-    }
-
-    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-    {
-      return data.GetEnumerator();
-    }
   }
 }
---
+++ Ivony.Html/Indexing/ElementNameIndex.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ivony.Fluent;
+
+
+namespace Ivony.Html.Indexing
+{
+
+  /// <summary>
+  /// 元素名称的索引
+  /// </summary>
+  public class ElementNameIndex
+  {
+
+
+    private IHtmlDocument _document;
+
+    /// <summary>
+    /// 创建元素名称的索引
+    /// </summary>
+    /// <param name="document"></param>
+    public ElementNameIndex( IHtmlDocument document )
+    {
+      _document = document;
+    }
+
+
+    private IDictionary<string,List<IHtmlElement>> data;
+
+
+    public void Rebuild()
+    {
+      data = new Dictionary<string, List<IHtmlElement>>();
+
+      _document.Descendants().ForAll( element => IndexElement( element ) );
+
+    }
+
+    private void IndexElement( IHtmlElement element )
+    {
+      var name = element.Name;
+
+      IndexElement( name, element );
+
+    }
+
+
+
+    private void IndexElement( string name, IHtmlElement element )
+    {
+      var set = data[name] as List<IHtmlElement>;
+      if ( set == null )
+        set = data[name] = new List<IHtmlElement>();
+
+      set.Add( element );
+    }
+
+  }
+}

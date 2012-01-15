75f2023ad3cac95bab01d801287a72a7d0fbdb38
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2012-01-15 13:30:32 +0000

    添加 HtmlCommentRenderType 枚举
    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@85319 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Ivony.Html/IHtmlComment.cs
+++ Ivony.Html/IHtmlComment.cs
@@ -19,4 +19,21 @@ namespace Ivony.Html
       get;
     }
   }
+
+
+  /// <summary>
+  /// HTML 注释的类别
+  /// </summary>
+  public enum HtmlCommentRenderType
+  {
+    /// <summary>普通注释，使用 &lt;!--内容--&gt; 表示，输出时自动格式化</summary>
+    Normal,
+
+    /// <summary>原样输出，输出时保持原样输出，但不会被任何解析器理解为文本</summary>
+    AsIs,
+
+    /// <summary>不会输出的注释，仅存在于 DOM 结构中，无法输出</summary>
+    Unable
+  }
+
 }

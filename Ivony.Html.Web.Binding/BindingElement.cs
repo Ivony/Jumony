c5facd641faf71bcaf8f8be000db4eb4fe53fe21
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2012-05-23 10:09:39 +0000

    Web Binding 实验性项目启动
    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@90850 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html.Web.Binding/BindingElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html.Web.Binding
+{
+  
+
+  /*
+   * <binding attr="abc" viewdata=""
+   * 
+   * 
+  */
+  
+  public class BindingElement
+  {
+
+
+  }
+}
---
+++ Ivony.Html.Web.Binding/BindingManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html.Web.Binding
+{
+  public class BindingManager
+  {
+
+
+    public IHtmlDocument Document
+    {
+      get;
+      private set;
+    }
+
+    public BindingManager( IHtmlDocument document )
+    {
+      Document = document;
+    }
+
+
+
+
+  }
+}

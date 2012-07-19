f59540624a16b531d011b339e7535040d5ccb4f6
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2012-07-19 07:56:04 +0000

    完善FreeNode兼容模型
    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@92969 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html.LegacyDomModel/FreeElement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html
+{
+  public sealed class FreeElement : FreeNode, IFreeElement
+  {
+
+    internal FreeElement( IHtmlFragment fragment, IHtmlNodeFactory factory )
+      : base( fragment, factory )
+    {
+      Element = Node as IHtmlElement;
+      if ( Element == null )
+        throw new InvalidOperationException();
+    }
+
+
+    private IHtmlElement Element
+    {
+      get;
+      set;
+    }
+
+
+    public string Name
+    {
+      get { return Element.Name; }
+    }
+
+    public IEnumerable<IHtmlAttribute> Attributes()
+    {
+      return Element.Attributes();
+    }
+
+    public IEnumerable<IHtmlNode> Nodes()
+    {
+      return Element.Nodes();
+    }
+
+    public object SyncRoot
+    {
+      get { return Element.SyncRoot; }
+    }
+  }
+}
---
+++ Ivony.Html.LegacyDomModel/FreeNode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html
+{
+  public class FreeNode : IFreeNode
+  {
+    internal FreeNode( IHtmlFragment fragment, IHtmlNodeFactory factory )
+    {
+      _fragment = fragment;
+      _factory = factory;
+      Node = _fragment.Nodes().First();
+    }
+
+    IHtmlFragment _fragment;
+    IHtmlNodeFactory _factory;
+
+    protected IHtmlNode Node
+    {
+      get;
+      private set;
+    }
+
+    public IHtmlNode Into( IHtmlContainer container, int index )
+    {
+      return _fragment.Into( container, index ).First();
+    }
+
+    public IHtmlNodeFactory Factory
+    {
+      get { return _factory; }
+    }
+
+    public IHtmlContainer Container
+    {
+      get { return _fragment; }
+    }
+
+    public object RawObject
+    {
+      get { return Node.RawObject; }
+    }
+
+    public string RawHtml
+    {
+      get { return Node.RawHtml; }
+    }
+
+    public IHtmlDocument Document
+    {
+      get { return _fragment.Document; }
+    }
+  }
+}

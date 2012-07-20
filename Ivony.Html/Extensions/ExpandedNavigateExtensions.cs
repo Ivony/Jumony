e361472aa0c29f04544e7b220706a07483b753cf
SND\Ivony_cp <SND\Ivony_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2012-07-20 10:35:23 +0000

    扩展API
    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@93051 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ Ivony.Html/Extensions/ExpandedNavigateExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ivony.Fluent;
+
+namespace Ivony.Html.ExpandedAPI
+{
+  public static class ExpandedNavigateExtensions
+  {
+
+    public static IEnumerable<IHtmlNode> Nodes( IHtmlContainer container, Action<IHtmlNode> action )
+    {
+      return container.Nodes().ForAll( action );
+    }
+
+    public static IEnumerable<IHtmlElement> Elements( IHtmlContainer container, Action<IHtmlElement> action )
+    {
+      return container.Elements().ForAll( action );
+    }
+
+    public static IEnumerable<IHtmlElement> Elements( IHtmlContainer container, string selector, Action<IHtmlElement> action )
+    {
+      return container.Elements( selector ).ForAll( action );
+    }
+
+    public static IEnumerable<IHtmlElement> Descendants( IHtmlContainer container, Action<IHtmlElement> action )
+    {
+      return container.Descendants().ForAll( action );
+    }
+
+    public static IEnumerable<IHtmlElement> Descendants( IHtmlContainer container, string selector, Action<IHtmlElement> action )
+    {
+      return container.Descendants( selector ).ForAll( action );
+    }
+
+    public static IEnumerable<IHtmlNode> DescendantNodes( IHtmlContainer container, Action<IHtmlNode> action )
+    {
+      return container.DescendantNodes().ForAll( action );
+    }
+
+    public static IEnumerable<IHtmlElement> Siblings( IHtmlNode node, Action<IHtmlElement> action )
+    {
+      return node.Siblings().ForAll( action );
+    }
+
+    public static IEnumerable<IHtmlElement> Siblings( IHtmlNode node, string selector, Action<IHtmlElement> action )
+    {
+      return node.Siblings( selector ).ForAll( action );
+    }
+
+  }
+}

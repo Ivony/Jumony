dda87a1db05035e7a6e9ffabe45ba7179f73af1c
SND\cnlangzi_cp <SND\cnlangzi_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2012-11-30 01:18:04 +0000

    Added an unit test to test find a css class which name contains a hyphen.
    git-svn-id: https://jumony.svn.codeplex.com/svn/trunk@96398 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ SelectorTest/CssClassSelectorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ivony.Html;
+using Ivony.Html.Parser;
+
+
+namespace SelectorTest
+{
+    [TestClass]
+    public class CssClassSelectorTest
+    {
+        [TestMethod]
+        public void css_class_has_hyphen()
+        {
+            var html = "<div class=\"css_class\"></div>";
+            var htmlParser = new JumonyParser();
+            var doc = htmlParser.Parse(html);
+
+            var css_class = doc.Find(".css_class");
+
+            Assert.AreEqual(1, css_class.Count());
+        }
+    }
+}

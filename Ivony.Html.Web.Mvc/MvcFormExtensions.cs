ecaea4ed3ad2eca5b45af69e7b248ba7c4f64995
Ivony <Ivony@live.com>
2013-01-17 12:07:50 +0800

    增加针对 MVC 的表单扩展

---
+++ Ivony.Html.Web.Mvc/MvcFormExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Ivony.Fluent;
+using Ivony.Html.Forms;
+
+namespace Ivony.Html.Web.Mvc
+{
+  public static class MvcFormExtensions
+  {
+
+    public static HtmlForm ApplyValues( this HtmlForm form, object dataModel )
+    {
+
+      var data = dataModel.ToPropertiesMap();
+
+      foreach ( var key in form.InputControls.Select( c => c.Name ) )
+      {
+
+        if ( data.ContainsKey( key ) )
+          form[key].TrySetValue( data[key] );
+
+      }
+
+      return form;
+
+    }
+
+
+    public static HtmlForm ApplyValues( this HtmlForm form, IValueProvider valueProvider )
+    {
+
+      foreach ( var key in form.InputControls.Select( c => c.Name ) )
+      {
+        if ( valueProvider.ContainsPrefix( key ) )
+        {
+          form[key].TrySetValue( valueProvider.GetValue( key ).AttemptedValue );
+        }
+      }
+
+      return form;
+
+    }
+
+  }
+}

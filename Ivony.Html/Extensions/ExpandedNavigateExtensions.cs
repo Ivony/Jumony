3c11f87af87e9aacedb517ab9fee27fb405f7b2d
Ivony <Ivony@live.com>
2013-01-29 13:43:05 +0800

    更新扩展 API

--- Ivony.Html/Extensions/ExpandedNavigateExtensions.cs
+++ Ivony.Html/Extensions/ExpandedNavigateExtensions.cs
@@ -10,39 +10,6 @@ namespace Ivony.Html.ExpandedNavigateAPI
   {
 
     /// <summary>
-    /// 从当前容器按照 CSS 选择器搜索符合要求的唯一元素，如果有多个元素符合要求，则会引发异常。
-    /// </summary>
-    /// <param name="container">要搜索子代元素的容器</param>
-    /// <param name="expression">CSS选择器</param>
-    /// <returns>搜索到的符合要求的唯一元素</returns>
-    public static IHtmlElement FindSingle( this IHtmlContainer container, string expression )
-    {
-      if ( container == null )
-        throw new ArgumentNullException( "container" );
-
-      if ( expression == null )
-        throw new ArgumentNullException( "expression" );
-
-
-      IHtmlElement result;
-
-      try
-      {
-        result = container.Find( expression ).SingleOrDefault();
-      }
-      catch ( InvalidOperationException e )
-      {
-        throw new InvalidOperationException( string.Format( "符合选择器 \"{0}\" 的元素不唯一", expression ), e );
-      }
-
-      if ( result == null )
-        throw new InvalidOperationException( string.Format( "未找到符合选择器 \"{0}\" 的元素。", expression ) );
-
-      return result;
-    }
-
-
-    /// <summary>
     /// 从当前容器按照 CSS 选择器搜索符合要求的唯一元素，如果有多个元素符合要求，则会引发异常，如果没有符合要求的元素，则返回 defaultElement 或者 null 。
     /// </summary>
     /// <param name="container">要搜索子代元素的容器</param>
@@ -69,34 +36,6 @@ namespace Ivony.Html.ExpandedNavigateAPI
     }
 
 
-
-    /// <summary>
-    /// 从当前容器按照 CSS 选择器搜索符合要求的第一个元素，若不存在任何符合要求的元素，则抛出异常。
-    /// </summary>
-    /// <param name="container">要搜索子代元素的容器</param>
-    /// <param name="expression">CSS选择器</param>
-    /// <returns>搜索到的符合要求的第一个元素</returns>
-    public static IHtmlElement FindFirst( this IHtmlContainer container, string expression )
-    {
-      if ( container == null )
-        throw new ArgumentNullException( "container" );
-
-      if ( expression == null )
-        throw new ArgumentNullException( "expression" );
-
-
-
-      IHtmlElement result;
-
-      result = container.Find( expression ).FirstOrDefault();
-
-      if ( result == null )
-        throw new InvalidOperationException( string.Format( "未找到符合选择器 \"{0}\" 的元素。", expression ) );
-
-      return result;
-    }
-
-
     /// <summary>
     /// 从当前容器按照 CSS 选择器搜索符合要求的第一个元素，若不存在任何符合要求的元素，则返回 defaultElement 或者 null 。
     /// </summary>
@@ -114,34 +53,6 @@ namespace Ivony.Html.ExpandedNavigateAPI
 
 
       return container.Find( expression ).FirstOrDefault() ?? defaultElement;
-    }
-
-
-
-    /// <summary>
-    /// 从当前容器按照 CSS 选择器搜索符合要求的最后一个元素，若不存在任何符合要求的元素，则抛出异常。
-    /// </summary>
-    /// <param name="container">要搜索子代元素的容器</param>
-    /// <param name="expression">CSS选择器</param>
-    /// <returns>搜索到的符合要求的最后一个元素</returns>
-    public static IHtmlElement FindLast( this IHtmlContainer container, string expression )
-    {
-      if ( container == null )
-        throw new ArgumentNullException( "container" );
-
-      if ( expression == null )
-        throw new ArgumentNullException( "expression" );
-
-
-
-      IHtmlElement result;
-
-      result = container.Find( expression ).LastOrDefault();
-
-      if ( result == null )
-        throw new InvalidOperationException( string.Format( "未找到符合选择器 \"{0}\" 的元素。", expression ) );
-
-      return result;
     }
 
 
@@ -164,18 +75,6 @@ namespace Ivony.Html.ExpandedNavigateAPI
       return container.Find( expression ).FirstOrDefault() ?? defaultElement;
     }
 
-
-
-    /// <summary>
-    /// 确认在当前容器存在 CSS 选择器搜索符合要求的元素，若找到，则返回 true 。
-    /// </summary>
-    /// <param name="container">要搜索子代元素的容器</param>
-    /// <param name="expression">CSS选择器</param>
-    /// <returns>是否存在符合要求的元素</returns>
-    public static bool Exists( this IHtmlContainer container, string expression )
-    {
-      return container.Find( expression ).Any();
-    }
 
 
 

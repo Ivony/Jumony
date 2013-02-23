40e9a573495384ce3c4f18a0fa2d2bf16421f8ab
Ivony <Ivony@live.com>
2013-02-23 14:21:55 +0800

    增强 JumonyViewHandler ，提供各种 Find 和 For 方法

--- Ivony.Html.Web.Mvc/JumonyViewHandler.cs
+++ Ivony.Html.Web.Mvc/JumonyViewHandler.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web;
+using Ivony.Fluent;
+using Ivony.Html.ExpandedAPI;
 
 namespace Ivony.Html.Web.Mvc
 {
@@ -22,6 +24,71 @@ namespace Ivony.Html.Web.Mvc
     }
 
     #endregion
+
+
+    /// <summary>
+    /// 在处理范畴内查找符合选择器的元素
+    /// </summary>
+    /// <param name="expression">CSS 选择器表达式</param>
+    /// <returns>符合选择器的元素</returns>
+    protected IEnumerable<IHtmlElement> Find( string expression )
+    {
+      return Scope.Find( expression );
+    }
+
+    
+    /// <summary>
+    /// 在处理范畴内查找符合选择器的唯一元素
+    /// </summary>
+    /// <param name="expression">CSS 选择器表达式</param>
+    /// <returns>符合选择器的唯一元素</returns>
+    protected IHtmlElement FindSingle( string expression )
+    {
+      return Scope.FindSingle( expression );
+    }
+
+
+    /// <summary>
+    /// 在处理范畴内查找符合选择器的首个元素
+    /// </summary>
+    /// <param name="expression">CSS 选择器表达式</param>
+    /// <returns>符合选择器的首个元素</returns>
+    protected IHtmlElement FindFirst( string expression )
+    {
+      return Scope.FindFirst( expression );
+    }
+
+
+    /// <summary>
+    /// 对处理范畴内查找符合选择器的首个元素进行处理
+    /// </summary>
+    /// <param name="expression">CSS 选择器表达式</param>
+    /// <param name="action">要进行的处理</param>
+    protected void ForFirst( string expression, Action<IHtmlElement> action )
+    {
+      Scope.Find( expression ).ForFirst( action );
+    }
+
+    /// <summary>
+    /// 对处理范畴内查找符合选择器的唯一元素进行处理
+    /// </summary>
+    /// <param name="expression">CSS 选择器表达式</param>
+    /// <param name="action">要进行的处理</param>
+    protected void ForSingle( string expression, Action<IHtmlElement> action )
+    {
+      Scope.Find( expression ).ForSingle( action );
+    }
+
+
+    /// <summary>
+    /// 对处理范畴内查找符合选择器的所有元素进行处理
+    /// </summary>
+    /// <param name="expression">CSS 选择器表达式</param>
+    /// <param name="action">要进行的处理</param>
+    protected void ForAll( string expression, Action<IHtmlElement> action )
+    {
+      Scope.Find( expression ).ForAll( action );
+    }
 
   }
 }

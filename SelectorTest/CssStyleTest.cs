c4b8620633076d0a10effdba113b6597e0953b07
Ivony <Ivony@live.com>
2013-02-28 14:35:03 +0800

    修正 StyleSetting 测试文件路径错误

--- SelectorTest/CssStyleTest.cs
+++ SelectorTest/CssStyleTest.cs
@@ -13,7 +13,7 @@ namespace SelectorTest
     public void StyleSettingParseTest()
     {
 
-      var document = new JumonyParser().LoadDocument( Path.Combine( Environment.CurrentDirectory, "SelectorTest1.html" ) );
+      var document = new JumonyParser().LoadDocument( Path.Combine( Environment.CurrentDirectory, "CssStyleSettingTest1.html" ) );
 
       Assert.AreEqual( document.FindSingle( "div" ).Style().GetValue( "display" ), "none", "无法正确解析丢失分号的属性值" );
 

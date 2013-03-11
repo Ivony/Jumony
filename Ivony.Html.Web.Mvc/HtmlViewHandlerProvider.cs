870a858e2104e9f9136848e9f40c5196c0834372
Ivony <Ivony@live.com>
2013-03-11 08:43:37 +0800

    增设 HtmlViewHandlerProvider 类型，负责查找 IHtmlViewHandler

---
+++ Ivony.Html.Web.Mvc/HtmlViewHandlerProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Compilation;
+using System.Web.Hosting;
+
+namespace Ivony.Html.Web.Mvc
+{
+  internal static class HtmlViewHandlerProvider
+  {
+
+    public IHtmlViewHandler GetHandler( string virtualPath )
+    {
+      var handlerPath = virtualPath + ".ashx";
+
+      if ( HostingEnvironment.VirtualPathProvider.FileExists( handlerPath ) )
+      {
+        try
+        {
+          var handler = BuildManager.GetCompiledType( handlerPath ) as IHtmlViewHandler;
+          if ( handler != null )
+            return handler;
+        }
+        catch
+        {
+
+        }
+      }
+
+      return null;
+    }
+
+  }
+}

f2cb3f731b2f5362a09994e691dfc478f6e691fc
Ivony <Ivony@live.com>
2013-03-30 17:50:21 +0800

    优化 CSS 样式支持

--- Ivony.Html/Css/CssStyleSpecificationBase.cs
+++ Ivony.Html/Css/CssStyleSpecificationBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
@@ -8,6 +9,30 @@ namespace Ivony.Html.Css
   public abstract class CssStyleSpecificationBase
   {
 
+    
+
+
+    public CssStyleProperty[] TransformProperties( CssStyleProperty[] properties )
+    {
+    }
+
 
   }
+
+  public class Css21StyleSpecification : CssStyleSpecificationBase
+  {
+
+  }
+
+  public class CssStyleShorthandRuleCollection : KeyedCollection<string, ICssStyleShorthandRule>
+  {
+    protected override string GetKeyForItem( ICssStyleShorthandRule item )
+    {
+      return item.Name;
+    }
+  }
+
+
+  
+
 }

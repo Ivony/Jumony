a90e0f251114821905762f745a8eec7d00e284a9
Ivony <Ivony@live.com>
2013-08-23 10:32:43 +0800

    增加 CssSpecificity 类型

---
+++ Ivony.Html/Css/ICssSelector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html
+{
+  public interface ICssSelector
+  {
+
+    ISelector Selector { get; }
+
+    CssSpecificity Specificity { get; }
+
+  }
+}

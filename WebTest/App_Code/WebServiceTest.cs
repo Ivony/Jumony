18d5f581ddd4e3e0a3d89c67acdc9b867e35718c
Ivony <Ivony@live.com>
2013-11-05 21:16:08 +0800

    新增各种 Web 测试

--- WebTest/App_Code/WebServiceTest.cs
+++ WebTest/App_Code/WebServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using Ivony.Web;
+﻿using Ivony.Html.Web;
+using Ivony.Web;
 using Ivony.Web.Test;
 using System;
 using System.Collections.Generic;
@@ -16,18 +17,50 @@ namespace WebTest
       var service2 = "~/test/";
       var service3 = "~/";
 
+      var globalService = "global";
+
       WebServices.RegisterService( service1, "~/test/1.html" );
       WebServices.RegisterService( service2, "~/test/" );
       WebServices.RegisterService( service3, "~/" );
+      WebServices.RegisterService( globalService );
 
-      var services = WebServices.GetServices<object>( "~/test/1.html" );
+      var services = WebServices.GetServices<string>( "~/test/1.html" );
+
 
       Assert.Contains( services, service1, "注册服务在页面失败" );
       Assert.Contains( services, service2, "注册服务在目录失败" );
       Assert.Contains( services, service3, "注册服务在根目录失败" );
+      Assert.Contains( services, globalService, "注册全局服务失败" );
+    }
 
+    public void ContentService()
+    {
+
+      WebServices.RegisterService( new TestContentService(), VirtualPathUtility.GetDirectory( testContentPath ) );
+      var result = HtmlProviders.LoadContent( testContentPath );
+
+      Assert.AreEqual( result.Content, testContent, "测试内容提供程序失败" );
 
     }
+
+
+    private const string testContentPath = "~/test/content/1.html";
+    private const string testContent = "Hello World";
+
+    private class TestContentService : IHtmlContentProvider
+    {
+
+
+      public HtmlContentResult LoadContent( string virtualPath )
+      {
+        if ( virtualPath == testContentPath )
+          return new HtmlContentResult( this, testContent, virtualPath );
+
+        else
+          return null;
+      }
+    }
+
 
   }
 }

f55467ccbfd0b8839250a26262f1652ea5ab1aea
Ivony <Ivony@live.com>
2013-11-09 15:06:24 +0800

    增补注释

--- Ivony.Html.Web/JumonyRequestMapperRoute.cs
+++ Ivony.Html.Web/JumonyRequestMapperRoute.cs
@@ -7,8 +7,18 @@ using System.Web.Routing;
 
 namespace Ivony.Html.Web
 {
+
+  /// <summary>
+  /// 辅助实现 RequestMapping 机制的路由
+  /// </summary>
   public class JumonyRequestMapperRoute : RouteBase
   {
+
+    /// <summary>
+    /// 获取路由信息，将对请求进行 RequestMapping 的结果包装成路由信息
+    /// </summary>
+    /// <param name="httpContext">当前 HTTP 请求上下文</param>
+    /// <returns>路由信息</returns>
     public override RouteData GetRouteData( HttpContextBase httpContext )
     {
 
@@ -25,6 +35,13 @@ namespace Ivony.Html.Web
 
     }
 
+
+    /// <summary>
+    /// 获取虚拟路径，此方法总是返回 null
+    /// </summary>
+    /// <param name="requestContext">当前请求上下文</param>
+    /// <param name="values">路由值</param>
+    /// <returns>虚拟路径数据，总是返回 null</returns>
     public override VirtualPathData GetVirtualPath( RequestContext requestContext, RouteValueDictionary values )
     {
       return null;

56ac3d90168b0e60efee460191c57e8e435a6784
Ivony <Ivony@live.com>
2013-11-30 23:27:51 +0800

    整合测试用例

--- APITest/StyleAPITest.cs
+++ APITest/StyleAPITest.cs
@@ -4,6 +4,7 @@ using Ivony.Html;
 using Ivony.Html.Styles;
 using Ivony.Html.Parser;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace APITest
 {
@@ -95,6 +96,20 @@ namespace APITest
       Assert.IsFalse( CssParser.Create( element.Document, ".deleted.completed" ).IsEligible( element ), ".Class( +name, ~name )" );
       Assert.IsTrue( CssParser.Create( element.Document, ".deleted" ).IsEligible( element ), ".Class( +name, ~name )" );
 
+    }
+
+    [TestMethod]
+    public void StyleParseTest()
+    {
+      var document = new JumonyParser().LoadDocument( Path.Combine( Environment.CurrentDirectory, "CssStyleSettingTest1.html" ) );
+
+      Assert.AreEqual( document.FindFirst( "div" ).Style().GetValue( "display" ), "none", "无法正确解析丢失分号的属性值" );
+      Assert.AreEqual( document.FindFirst( "a" ).Style().GetValue( "display" ), null, "无法正确解析没有style属性的元素" );
+      Assert.AreEqual( document.FindFirst( "p" ).Style().GetValue( "width" ), "12px", "无法正确解析空白样式设置值后随的正确表达式" );
+      Assert.AreEqual( document.FindFirst( "p" ).Style().GetValue( "display" ), null, "无法正确解析空白样式设置值" );
+
+      var element = document.FindFirst( "div#test1" );
+      Assert.AreEqual( element.Style().GetValue( "display" ), "none", "CSS 属性设置不应区分大小写" );
     }
 
   }

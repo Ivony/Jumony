786982fcdca8466356f2407a939ef7a1b089d476
Ivony <Ivony@live.com>
2013-12-03 23:49:03 +0800

    HtmlRequestContext 对象增对象构造函数增加参数检查

--- Ivony.Html.Web/HtmlRequestContext.cs
+++ Ivony.Html.Web/HtmlRequestContext.cs
@@ -16,12 +16,26 @@ namespace Ivony.Html.Web
 
     public HtmlRequestContext( HttpContextBase httpContext, string virtualPath )
     {
+
+      if ( httpContext == null )
+        throw new ArgumentNullException( "httpContext" );
+
+      if ( virtualPath == null )
+        throw new ArgumentNullException( "virtualPath" );
+
       HttpContext = httpContext;
       VirtualPath = virtualPath;
     }
 
+    /// <summary>
+    /// 当前 HTTP 请求
+    /// </summary>
     public HttpContextBase HttpContext { get; private set; }
 
+
+    /// <summary>
+    /// 当前处理的文档的虚拟路径
+    /// </summary>
     public string VirtualPath { get; private set; }
 
   }

d8cdecc29db52c39fb33bfe283f5e6946ce85b05
Ivony <Ivony@live.com>
2014-02-20 18:09:32 +0800

    整合 JumonySite ，进一步拆分 Area

--- Jumony.Demo.HelpCenter/HelpController.cs
+++ Jumony.Demo.HelpCenter/HelpController.cs
@@ -20,6 +20,9 @@ namespace Jumony.Demo.HelpCenter
   public class HelpController : Controller
   {
 
+    private const string helpEntriesVirtualPath = "~/HelpEntries/";
+
+
 
 
     public ActionResult Entry( string path = "." )
@@ -34,8 +37,64 @@ namespace Jumony.Demo.HelpCenter
 
     public ActionResult Navigation( string path = "." )
     {
+      var parents = GetParents( path ).Select( GetTopic );
+
+
       return PartialView( "Navigation", HelpTopic.GetTopic( path ) );
     }
 
+
+    private static readonly string _topicCachePrefix = "Help_Topic_";
+
+    private Topic GetTopic( string path )
+    {
+
+      var cacheKey = _topicCachePrefix + path;
+
+      var topic = HttpRuntime.Cache.Get( cacheKey ) as Topic;
+
+      if ( topic == null )
+      {
+        CacheDependency dependency;
+        IHtmlDocument document;
+
+        if ( path.EndsWith( "/" ) )
+          document = HtmlServices.LoadDocument( path + "index.html", out dependency );
+        else
+          document = HtmlServices.LoadDocument( path, out dependency );
+
+        var title = document.FindFirst( "title" ).InnerText();
+        topic = new Topic
+        {
+          VirtualPath = path,
+          Title = title
+        };
+
+        if ( dependency != null )
+          HttpRuntime.Cache.Insert( cacheKey, topic, dependency );
+      }
+
+      return topic;
+    }
+
+    private IEnumerable<string> GetParents( string path )
+    {
+
+      if ( path.EqualsIgnoreCase( helpEntriesVirtualPath ) )
+        yield break;
+
+      var parent = VirtualPathHelper.GetParentDirectory( path );
+      yield return parent;
+
+      foreach ( var _path in GetParents( parent ) )
+        yield return _path;
+    }
+  }
+
+  public class Topic
+  {
+    public string VirtualPath { get; set; }
+
+    public string Title { get; set; }
   }
 }

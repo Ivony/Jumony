cf6bb98c91751b6e447a5c08de61f7a43f9d6a21
Ivony <Ivony@live.com>
2014-03-26 18:48:45 +0800

    新增 DataAnnotationMetadataProvider

---
+++ Ivony.Html.Forms/DataAnnotationMeatadataProvider.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html.Forms
+{
+  public class DataAnnotationMeatadataProvider : IFormMetadataProvider
+  {
+
+
+
+    public DataAnnotationMeatadataProvider( Type modelType )
+    {
+      ModelType = modelType;
+    }
+
+    protected Type ModelType
+    {
+      get;
+      private set;
+    }
+
+
+    private ICustomTypeDescriptor _typeDescriptor;
+
+    protected virtual ICustomTypeDescriptor GetTypeDescriptor()
+    {
+      return new AssociatedMetadataTypeTypeDescriptionProvider( ModelType ).GetTypeDescriptor( ModelType );
+    }
+
+
+    protected ICustomTypeDescriptor TypeDescrptor
+    {
+      get
+      {
+        if ( _typeDescriptor == null )
+          _typeDescriptor = GetTypeDescriptor();
+
+        return _typeDescriptor;
+      }
+    }
+
+
+    public FormFieldMetadata GetFieldMetadata( string name )
+    {
+
+      
+      throw new NotImplementedException();
+    }
+
+
+    protected FormFieldMetadata GetFieldMetadata( PropertyDescriptor property )
+    {
+      throw new NotImplementedException();
+    }
+
+
+
+  }
+}

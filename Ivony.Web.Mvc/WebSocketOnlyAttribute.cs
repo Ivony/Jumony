fea3661ca349930035407cc13cedd02ce08aef36
Ivony <Ivony@live.com>
2014-04-01 23:49:53 +0800

    新增 Ivony.Web.Mvc 项目，为 ASP.NET MVC 提供一些扩展

---
+++ Ivony.Web.Mvc/WebSocketOnlyAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Ivony.Web
+{
+
+  /// <summary>
+  /// 标识 Action 只能处理 WebSocket 请求
+  /// </summary>
+  public class WebSocketOnlyAttribute : ActionMethodSelectorAttribute
+  {
+
+    /// <summary>
+    /// 重写 IsValidForRequest 方法，判断当前请求是否为 WebSocket 请求
+    /// </summary>
+    /// <param name="controllerContext">当前控制器上下文</param>
+    /// <param name="methodInfo">Action 方法信息</param>
+    /// <returns>当前请求是否为 WebSocket 请求</returns>
+    public override bool IsValidForRequest( ControllerContext controllerContext, MethodInfo methodInfo )
+    {
+      return controllerContext.HttpContext.IsWebSocketRequest;
+    }
+  }
+}
---
+++ Ivony.Web.Mvc/WebSocketResult.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebSockets;
+
+namespace Ivony.Web
+{
+
+  /// <summary>
+  /// 定义一个处理 WebSocket 请求的 Result
+  /// </summary>
+  public class WebSocketResult : ActionResult
+  {
+
+    /// <summary>
+    /// 创建 WebSocketResult 对象
+    /// </summary>
+    /// <param name="processer">处理函数</param>
+    public WebSocketResult( Func<AspNetWebSocketContext, Task> processer )
+    {
+      Processer = processer;
+    }
+
+
+    /// <summary>
+    /// 用于处理 WebSocket 请求的处理函数
+    /// </summary>
+    public Func<AspNetWebSocketContext, Task> Processer
+    {
+      get;
+      private set;
+    }
+
+
+    /// <summary>
+    /// 在当前上下文执行结果
+    /// </summary>
+    /// <param name="context">当前控制器上下文</param>
+    public override void ExecuteResult( ControllerContext context )
+    {
+
+      if ( !context.HttpContext.IsWebSocketRequest )
+        throw new HttpException( 406, "当前请求不是 WebSocket 请求" );
+
+      context.HttpContext.AcceptWebSocketRequest( Processer );
+    }
+  }
+}

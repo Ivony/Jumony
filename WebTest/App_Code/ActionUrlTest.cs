d36674646a8218890fd20fe11f4192fb94b7cbb2
Ivony <Ivony@live.com>
2014-04-29 15:15:07 +0800

    新增关于 Action URL 的单元测试。

--- WebTest/App_Code/ActionUrlTest.cs
+++ WebTest/App_Code/ActionUrlTest.cs
@@ -3,16 +3,72 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Routing;
+using Ivony.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Web.Mvc.Html;
+using Ivony.Html;
+using Ivony.Html.Parser;
 
 /// <summary>
 /// ActionUrlTest 的摘要说明
 /// </summary>
 public class ActionUrlTest : TestClass
 {
-  public ActionUrlTest()
+
+  public override void Initialize( HttpContext context )
   {
-    //
-    // TODO: 在此处添加构造函数逻辑
-    //
+    RouteTable.Routes.SimpleRouteTable().MapDefaultRoute();
+
+    if ( context == null )
+      throw new InvalidOperationException();
+
+    base.Initialize( context );
+  }
+
+
+  public override void MethodInitialize()
+  {
+    var routeData = HttpContext.Request.RequestContext.RouteData;
+    routeData.Values.Add( "controller", "Test" );
+    routeData.Values.Add( "action", "Test" );
+
+    base.MethodInitialize();
+  }
+
+  public void Test1()
+  {
+
+
+    var context = new ControllerContext( HttpContext.Request.RequestContext, new TestController() );
+
+    var result = ViewEngines.Engines.FindView( context, "~/ActionUrlTest/Test1.html", null );
+
+    Assert.NotNull( result.View, "找不到视图" );
+
+
+    IHtmlDocument document;
+
+    using ( var writer = new StringWriter() )
+    {
+      result.View.Render( new ViewContext( context, result.View, new ViewDataDictionary(), new TempDataDictionary(), writer ), writer );
+
+      document = new JumonyParser().Parse( writer.ToString() );
+    }
+
+
+    var link = document.FindFirst( "a" );
+
+    Assert.NotNull( link );
+
+    Assert.AreEqual( link.Attribute( "href" ).Value(), "/TestController/TestAction?arg=args" );
+
+
+  }
+
+  private class TestController : Controller
+  {
+
   }
 }

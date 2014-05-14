7b7e20599f8e6f6e9666deafb4f677c0421cd7d5
Ivony <Ivony@live.com>
2014-05-14 15:04:44 +0800

    新增 Ivony.Html.Controls 项目

---
+++ Ivony.Html.Controls/ControlRenderAdapter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivony.Html.Controls
+{
+  public class ControlRenderAdapter : IHtmlRenderAdapter
+  {
+    public bool Render( IHtmlNode node, IHtmlRenderContext context )
+    {
+
+      var element = node as IHtmlElement;
+      if ( element == null )              //只处理元素
+        return false;
+
+      var control = CreateControl( element );
+
+
+      throw new NotImplementedException();
+    }
+
+    private HtmlControl CreateControl( IHtmlElement element )
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Ivony.Html.Controls/HtmlControl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html.Controls
+{
+
+  /// <summary>
+  /// 定义 HTML 控件
+  /// </summary>
+  public abstract class HtmlControl
+  {
+
+    internal void ProcessControl( IHtmlElement element, IHtmlRenderContext renderContext )
+    {
+
+      HtmlElement = element;
+
+      Init();
+
+      Load();
+
+      RenderCore( renderContext );
+
+      Unload();
+
+    }
+
+    protected IHtmlElement HtmlElement { get; private set; }
+
+
+    protected virtual void Init()
+    {
+    }
+
+
+    protected virtual void Load()
+    {
+    }
+
+
+    protected virtual void RenderCore( IHtmlRenderContext renderContext )
+    {
+    }
+
+
+    protected virtual void Unload()
+    {
+    }
+
+  }
+}

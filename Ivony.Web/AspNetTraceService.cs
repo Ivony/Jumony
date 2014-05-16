b5168e65e3ef4980d22c963552ceb7a3ec1156fa
Ivony <Ivony@live.com>
2014-05-16 19:06:52 +0800

    新增 ITraceService

---
+++ Ivony.Web/AspNetTraceService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Web
+{
+  public class AspNetTraceService : ITraceService
+  {
+    public void Trace( TraceLevel level, string category, string message )
+    {
+
+      switch ( level )
+      {
+        case TraceLevel.Error:
+        case TraceLevel.Warning:
+
+          TraceWarning( category, message );
+          break;
+        case TraceLevel.Info:
+        case TraceLevel.Verbose:
+          TraceInfo( category, message );
+
+          break;
+        case TraceLevel.Off:
+        default:
+          break;
+      }
+    }
+
+    private void TraceInfo( string category, string message )
+    {
+      throw new NotImplementedException();
+    }
+
+    private void TraceWarning( string category, string message )
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Ivony.Web/ITraceService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Web
+{
+  public interface ITraceService
+  {
+
+    void Trace( TraceLevel level, string category, string message );
+
+  }
+}

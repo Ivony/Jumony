e1e48fa583c2ad84c986253e36cc98f40fa39441
Ivony <Ivony@live.com>
2014-05-20 11:50:29 +0800

    删除 JumonyUrlHelper 中所有失去意义的方法

--- Ivony.Html.Web.Mvc/JumonyUrlHelper.cs
+++ Ivony.Html.Web.Mvc/JumonyUrlHelper.cs
@@ -47,118 +47,12 @@ namespace Ivony.Html.Web
     /// 处理所有 Action 路由
     /// </summary>
     /// <param name="container">包含所有要处理元素的 HTML 容器</param>
-    internal void ProcessActionUrls( IHtmlContainer container )
-    {
-      var elements = container.Find( "a[action] , img[action] , form[action][controller] , script[action]" );
-
-      foreach ( var actionElement in elements )
-      {
-
-        lock ( actionElement.SyncRoot )//锁住元素不被修改
-        {
-
-          var action = actionElement.Attribute( "action" ).Value() ?? RouteData.Values["action"].CastTo<string>();
-          var controller = actionElement.Attribute( "controller" ).Value() ?? RouteData.Values["controller"].CastTo<string>();
-
-
-          var routeValues = GetRouteValues( actionElement );
-
-
-          actionElement.RemoveAttribute( "action" );
-          actionElement.RemoveAttribute( "controller" );
-          actionElement.RemoveAttribute( "inherits" );
-
-
-          var url = Url.Action( action, controller, routeValues );
-
-
-          string attributeName;
-          switch ( actionElement.Name.ToLowerInvariant() )
-          {
-            case "a":
-              attributeName = "href";
-              break;
-            case "form":
-              attributeName = "action";
-              break;
-            case "img":
-            case "script":
-              attributeName = "src";
-              break;
-
-            default:
-              throw new Exception();//不可能出现的错误
-          }
-
-          if ( url == null )
-            actionElement.Attribute( attributeName ).Remove();
-
-          else
-            actionElement.SetAttribute( attributeName, url );
-
-        }
-      }
-    }
 
 
     private Dictionary<IHtmlElement, IDictionary<string, string>> elementRouteValuesTable = new Dictionary<IHtmlElement, IDictionary<string, string>>();
 
 
-    /// <summary>
-    /// 为指定元素设置路由参数值，此设置会覆盖元素属性上同名路由参数的设置。
-    /// </summary>
-    /// <param name="element">要设置路由参数的元素</param>
-    /// <param name="routeValues">要设置的路由参数</param>
-    /// <exception cref="System.NotSupportedException">元素没有支持路由的属性</exception>
-    public void SetRouteValues( IHtmlElement element, IDictionary<string, string> routeValues )
-    {
 
-    }
-
-
-
-    /// <summary>
-    /// 从元素标签中获取路由的虚拟路径
-    /// </summary>
-    /// <param name="element">要获取分析路由虚拟路径的元素</param>
-    /// <returns>获取的虚拟路径</returns>
-    public string GetRouteUrl( IHtmlElement element )
-    {
-      return GetRouteUrl( element, true );
-    }
-
-
-    /// <summary>
-    /// 从元素标签中获取路由的虚拟路径
-    /// </summary>
-    /// <param name="element">要获取分析路由虚拟路径的元素</param>
-    /// <param name="clearRouteAttributes">是否清理路由属性设置</param>
-    /// <returns>获取的虚拟路径</returns>
-    internal string GetRouteUrl( IHtmlElement element, bool clearRouteAttributes )
-    {
-
-      if ( element == null )
-        throw new ArgumentNullException( "element" );
-
-      if ( element.Attribute( "action" ) == null )
-        throw new InvalidOperationException();
-
-      var action = element.Attribute( "action" ).Value() ?? RouteData.Values["action"].CastTo<string>();
-      var controller = element.Attribute( "controller" ).Value() ?? RouteData.Values["controller"].CastTo<string>();
-
-
-      var routeValues = GetRouteValues( element, clearRouteAttributes );
-
-
-      if ( clearRouteAttributes )
-      {
-        element.RemoveAttribute( "action" );
-        element.RemoveAttribute( "controller" );
-        element.RemoveAttribute( "inherits" );
-      }
-
-      return Url.Action( action, controller, routeValues );
-    }
 
 
     /// <summary>
@@ -260,31 +154,6 @@ namespace Ivony.Html.Web
       return result;
     }
 
-
-    /// <summary>
-    /// 转换容器中所有 URI 与当前请求匹配。
-    /// </summary>
-    /// <param name="container">确定要转换 URI 范围的容器</param>
-    public void ResolveUri( IHtmlContainer container )
-    {
-      ResolveUri( container, VirtualPath );
-    }
-
-    /// <summary>
-    /// 转换容器中所有 URI 与当前请求匹配。
-    /// </summary>
-    /// <param name="container">确定要转换 URI 范围的容器</param>
-    /// <param name="baseVirtualPath">容器的基路径</param>
-    public void ResolveUri( IHtmlContainer container, string baseVirtualPath )
-    {
-      var specification = container.Document.HtmlSpecification;
-
-      var absoluteBase = VirtualPathUtility.ToAbsolute( baseVirtualPath );
-      foreach ( var attribute in container.Descendants().SelectMany( e => e.Attributes() ).Where( a => specification.IsUriValue( a ) ).ToArray() )
-      {
-        ResolveUri( attribute, absoluteBase );
-      }
-    }
 
     /// <summary>
     /// 转换 URI 与当前请求匹配

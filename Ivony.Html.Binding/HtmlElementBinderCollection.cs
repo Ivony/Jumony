d58dc3bb783062450264ded44ab9528c51190b5c
Ivony <Ivony@live.com>
2014-05-22 15:51:09 +0800

    新增 HtmlElementBinderCollection 类型

---
+++ Ivony.Html.Binding/HtmlElementBinderCollection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Ivony.Html.Binding
+{
+
+
+  /// <summary>
+  /// 定义 IHtmlElementBinder 的容器
+  /// </summary>
+  public sealed class HtmlElementBinderCollection : KeyedCollection<string, IHtmlElementBinder>
+  {
+
+    /// <summary>
+    /// 创建 HtmlElementBinderCollection 对象
+    /// </summary>
+    internal HtmlElementBinderCollection() : base( StringComparer.OrdinalIgnoreCase ) { }
+
+    /// <summary>
+    /// 从 IHtmlElementBinder 对象中提取用作键的元素名称
+    /// </summary>
+    /// <param name="item">IHtmlElementBinder 对象</param>
+    /// <returns>用作键的元素名称</returns>
+    protected override string GetKeyForItem( IHtmlElementBinder item )
+    {
+      return item.ElementName;
+    }
+  }
+}
